540425e05eb77c96c7acb0b2afa0d490b1759007
Mathiarasi Nagaraj <mathiarasinagaraj05@gmail.com>
2024-05-22 16:58:43 +0530

    initial file upload

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from './Button.module.css'
+const Button = () => {
+  return (
+ <button>Click me</button>
+  )
+}
+
+export default Button
---
+++ src/pages/uploadUX/UploadUX.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UploadUX = () => {
+  return (
+    <div>UploadUX</div>
+  )
+}
+
+export default UploadUX
